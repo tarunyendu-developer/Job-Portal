57fca5ea90c1040764c4f91f71df8f5f30d6dd6a
tarunyendu-developer <tarunyendu2807@gmail.com>
2025-10-10 18:19:15 +0530

    feat: add base packages, HomeController, and Thymeleaf index view

---
+++ src/main/java/com/jobportal/JobPortalsProject/JobPortalsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jobportal.JobPortalsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobportal/JobPortalsProject/JobPortalsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobportal.JobPortalsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
